d945ce144845734181ca4d7bf8426e51f2e0d183
suraj9552 <surajpatil9552@gmail.com>
2020-11-06 11:49:04 +0530

    jobApplication modified

--- freelancerapp/src/test/java/org/cap/apps/freelancerapp/jobapplicationms/service/JobApplicationServiceImplTest.java
+++ freelancerapp/src/test/java/org/cap/apps/freelancerapp/jobapplicationms/service/JobApplicationServiceImplTest.java
@@ -1,45 +1,58 @@
 package org.cap.apps.freelancerapp.jobapplicationms.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
-import org.cap.apps.freelancerapp.exceptions.AdminNotFoundException;
-import org.cap.apps.freelancerapp.exceptions.CoverLetterNotFoundException;
 import org.cap.apps.freelancerapp.freelancerms.entities.Freelancer;
+import org.cap.apps.freelancerapp.jobapplicationms.entities.JobApplication;
 import org.cap.apps.freelancerapp.jobms.entities.Job;
 import org.cap.apps.freelancerapp.recruiterms.entities.Recruiter;
 import org.cap.apps.freelancerapp.skillms.entities.Skill;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-class JobApplicationServiceImplTest {
 
-	JobApplicationServiceImpl service;
+@ExtendWith({SpringExtension.class})
+@DataJpaTest
+@Import(JobApplicationServiceImpl.class)
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
+public class JobApplicationServiceImplTest {
 
-	EntityManager entityManager;
 
-	/* Testcase1 for coverletter not found for recruiter */
-
+	@Autowired
+	IJobApplicationService service;
+	
+	@Autowired
+	private EntityManager entityManager;
+	
 	@Test
-	public void testCoverLetterNotFoundException() {
-		Executable executable = new Executable() {
-			@Override
-			public void execute() throws Throwable {
-				Skill skill = new Skill("Java", "FullStack");
-				Recruiter postedBy = new Recruiter("Avichal", "Saxena", "root");
-				LocalDate postedDate = LocalDate.now();
-				Boolean active = true;
-				Boolean value = active;
-				Job job = new Job(skill, postedBy, postedDate, value);
-				Freelancer freelancer = new Freelancer("Suraj", "Patil", "root");
-				service.applyToJob(job, null, freelancer);
-			}
-		};
-		Assertions.assertThrows(CoverLetterNotFoundException.class, executable);
-
+	public void testApplytoJob() {
+		Skill skill = new Skill();
+		Recruiter postedBy=new Recruiter();
+		LocalDate postedDate = LocalDate.parse("2019-11-01");
+		Boolean active=true;
+		Job job=new Job( skill,  postedBy,  postedDate, active);
+		String coverLetter="I have good technical skill";
+		String fName="Aniket";
+		String lName="Aniket";
+		String pass="Aniket123";
+		Freelancer freelancer=new Freelancer(fName,lName,pass);
+		JobApplication jobApplication=service.applyToJob(job, coverLetter, freelancer);
+		TypedQuery<JobApplication>query=entityManager.createQuery("from JobApplication",JobApplication.class);
+		List<JobApplication> jobApplicationList=query.getResultList();
+		JobApplication storedJobApplication=jobApplicationList.get(0);
+		Assertions.assertEquals(jobApplication.getId(),storedJobApplication.getId());
+        Assertions.assertEquals(firstName,storedJobApplication.getFirstName());
+        Assertions.assertEquals(lastName,storedJobApplication.getLastName());
+        Assertions.assertEquals(age,storedJobApplication.getAge());
 	}
 }
